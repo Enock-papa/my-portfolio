db7014acc3e96ea05ac6710d871b9dcce08f67cb
Enock-papa <osiirenock11@gmail.com>
2022-05-30 09:14:46 +0300

    portfolio

---
+++ index.php
@@ -0,0 +1,255 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>my portfolio</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="styles.css">
+
+  
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top ">
+    <div class="container-fluid">
+
+      <div class="row justify-content-center">
+        <div class="col-xl-9 d-flex align-items-center justify-content-lg-between">
+          <h1 class="logo me-auto me-lg-0"><a href="index.html">Enock.</a></h1>
+          <!-- Uncomment below if you prefer to use an image logo -->
+          <!-- <a href="index.html" class="logo me-auto me-lg-0"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+          <nav id="navbar" class="navbar order-last order-lg-0">
+            <ul>
+              <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+              <li><a class="nav-link scrollto" href="#services">Services</a></li>
+              <li><a class="nav-link scrollto " href="#projects">Skills</a></li>
+              <li><a class="nav-link scrollto" href="#contact">Contacts</a></li>
+              
+              
+            </ul>
+            <i class="bi bi-list mobile-nav-toggle"></i>
+          </nav><!-- .navbar -->
+
+          
+        </div>
+      </div>
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex flex-column justify-content-center">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-md-6">
+          <h1>Hello I'm Enock.</h1>
+          <p style="color: white';">Contact for any Computer services.</p>
+          
+          
+          
+
+        </div>
+      </div>
+    </div>
+  </section><!-- End Hero -->
+  
+
+  
+
+  <!--Service Section-->
+  <section id="services">
+    <div class="services container">
+      <div class="service-top">
+        <h1 class="section-title">Serv<span>i</span>ces</h1>
+      </div>
+      <div class="service-bottom">
+        <div class="service-item">
+          <div class="icon"><img src="https://img.icons8.com/external-kmg-design-outline-color-kmg-design/32/000000/external-web-web-hosting-kmg-design-outline-color-kmg-design.png"/></div>
+          <h2>Web Design And Development.</h2>
+          <p>I am Full stack web developer doing both frontend, backend and even database administration</p>
+        </div>
+        <div class="service-item">
+          <div class="icon"><img src="https://img.icons8.com/ios-filled/50/000000/server.png"/></div>
+          <h2>Software and Maintenance</h2>
+          <p>These are much crucial for effective running of a computer system</p>
+        </div>
+        <div class="service-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/50/000000/services.png"/></div>
+          <h2>Computer Maintenance</h2>
+          <p>All services on the computer installation and maintance are all under one roof</p>
+        </div>
+        <div class="service-item">
+          <div class="icon"><img src="https://img.icons8.com/external-sketchy-juicy-fish/60/000000/external-cyber-cyber-security-sketchy-sketchy-juicy-fish-7.png"/></div>
+          <h2>IT Security</h2>
+          <p>Do not wait until you are attacked by the virus itself for computer security is also under this roof</p>
+        </div>
+        <div class="service-item">
+          <div class="icon"><img src="https://img.icons8.com/external-soft-fill-juicy-fish/60/000000/external-network-network-architecture-soft-fill-soft-fill-juicy-fish-4.png"/></div>
+          <h2>Network Installation and Maintenance</h2>
+          <p>The joy and the efficient operation of a computer is its networks. Therefore do not hesitate to hire a network specialist</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!--End of Service Section-->
+
+  <!--Projects section-->
+  <section id="projects">
+    <div class="projects container">
+      <div class="projects-header">
+        <h1 class="section-title">S<span>kills</span></h1>
+      </div>
+      <div class="all-projects">
+        <div class="projects-item">
+          <div class="project-info">
+            <h1>Cyber <span>Security</span></h1>
+          </div>
+          <div class="project-img">
+            <img src="images/cyber.webp" alt="img">
+          </div>
+        </div>
+        <div class="projects-item">
+          <div class="project-info">
+            <h1>Web Devptmt</h1>
+          </div>
+          <div class="project-img">
+            <img src="images/web.jpeg" alt="img">
+          </div>
+        </div>
+        <div class="projects-item">
+          <div class="project-info">
+            <h1>Python Programming</h1>
+          </div>
+          <div class="project-img">
+            <img src="images/python.jpg" alt="img">
+          </div>
+        </div>
+        <div class="projects-item">
+          <div class="project-info">
+            <h1>Database Management</h1>
+          </div>
+          <div class="project-img">
+            <img src="images/database.jpg" alt="img">
+          </div>
+        </div>
+        
+        <div class="projects-item">
+          <div class="project-info">
+            <h1>Networking services</h1>
+          </div>
+          <div class="project-img">
+            <img src="images/network.jpg" alt="img">
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!--End of Projects section-->
+
+  <!--Contact Section-->
+  <section id="contact">
+    <div class="contact container">
+      <div> <h1 class="section-title"> Contact <span>Info</span></h1></div>
+      <div class="contact-items">
+        <div class="contact-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/50/000000/phone--v1.png"/></div>
+          <div class="contact-info">
+            <h1>Phone</h1>
+            <h2>0768223680</h2>
+          </div>
+        </div>
+        <div class="contact-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/new-post.png"/></div>
+          <div class="contact-info">
+            <h1>Email</h1>
+            <h2><a href="mailto:osiirenock11@gmail.com">osiirenock11@gmail.com</a></h2>
+          </div>
+        </div>
+        <div class="contact-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/map-marker.png"/></div>
+          <div class="contact-info">
+            <h1>Address</h1>
+            <h2>P.o.Box 1125 30100 </h2>
+            <h2>Eldoret</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!--End of Contact Section-->
+  <hr>
+  
+  <!--footer-->
+  <section id="footer">
+    <div class="footer container">
+      <div class="brand"> <h1><span>E</span>nock <span>O</span>sir</h1></div>
+      <h2>Your Complete Cyber Solution Patner</h2>
+      <div class="social-icon">
+        <div class="social-item">
+          <a href="https://github.com/Enock-papa/"><img src="https://img.icons8.com/bubbles/100/000000/github.png"/></a>
+        </div>
+        <div class="social-item">
+          <a href="mailto:osiirenock11@gmail.com"><img src="https://img.icons8.com/bubbles/100/000000/gmail.png"/></a>
+        </div>
+        <div class="social-item">
+          <a href="#"><img src="https://img.icons8.com/bubbles/100/000000/twitter.png"/></a>
+        </div>
+        <div class="social-item">
+          <a href="#"><img src="https://img.icons8.com/bubbles/100/000000/youtube.png"/></a>
+        </div>
+      </div>
+      <p> &copy; 2022 Created by Enock</p>
+
+  <p style="font-size: 20px italics"><marquee>All your computer solutions are under one roof...</marquee></p>
+
+    </div>
+  </section>
+  <!--End of footer-->
+  <script src="./app.js"></script>
+<style>.footer,.generic-footer{margin-bottom:98px}@media (min-width:374px){.footer,.generic-footer{margin-bottom:78px}}@media (min-width:546px){.footer,.generic-footer{margin-bottom:56px}}@media (min-width:1055px){.footer,.generic-footer{margin-bottom:0}}.disclaimer{position:fixed;z-index:9999999;bottom:0;right:0;border-top:2px solid #ff5c62;text-align:center;font-size:14px;font-weight:400;background-color:#fff;padding:5px 10px 5px 10px}.disclaimer a:hover{text-decoration:underline}@media (min-width:1052px){.disclaimer{text-align:right;border-left:2px solid red;border-top-left-radius:10px}}@media (min-width:1920px){.disclaimer{width:60%}}
+
+</style>
+
+</body>
+</html>
+
+  
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/purecounter/purecounter.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
